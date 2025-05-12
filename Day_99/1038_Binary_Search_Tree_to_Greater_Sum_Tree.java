c0773a79f31d8e4b06fed2bd2834508abb316343
iamsubham8 <168450064+iamsubham8@users.noreply.github.com>
2025-05-12 17:32:23 +0530

    Day-99 Completed

---
+++ Day_99/1038_Binary_Search_Tree_to_Greater_Sum_Tree.java
@@ -0,0 +1,49 @@
+// 1038. Binary Search Tree to Greater Sum Tree
+// Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus the sum of all keys greater than the original key in BST.
+// As a reminder, a binary search tree is a tree that satisfies these constraints:
+// •	The left subtree of a node contains only nodes with keys less than the node's key.
+// •	The right subtree of a node contains only nodes with keys greater than the node's key.
+// •	Both the left and right subtrees must also be binary search trees.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+ https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/
+class Solution {
+    int sum = 0 ;
+    public TreeNode bstToGst(TreeNode root) {
+        helper(root);
+        return root ;
+    }
+
+    public void helper(TreeNode root){
+        if(root == null){
+            return ;
+        }
+        helper(root.right);
+        sum = sum + root.val;
+        root.val = sum ;
+        helper(root.left);
+    }
+}
+
+
+
+
+
+
+
+
